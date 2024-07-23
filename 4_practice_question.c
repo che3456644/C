49b96e348096406832da97184921ec316639fad4
Chetan Bhagat <cbhagat191@gmail.com>
2024-07-23 20:47:06 +0530

    Add files via upload

---
+++ 4_practice_question.c
@@ -0,0 +1,15 @@
+/*write a c program to calculate area of a rectangle
+a)using hard coded inputs */
+
+#include <stdio.h>
+
+int main()
+{
+   int length = 7,breadth = 7;
+   int area = length*breadth;
+   printf("the area of rectangle is %d",area);
+
+    return 0;
+}
+ 
+
