8c2a1ad653714225728cfb89f42a29294133ad96
Chetan Bhagat <cbhagat191@gmail.com>
2024-07-25 21:37:18 +0530

    Add files via upload

---
+++ 7_practice_question.c
@@ -0,0 +1,12 @@
+// write a program to convert celcius (centigrade degrees temperature to farenhit)
+#include <stdio.h>
+
+int main()
+{
+    float celsius = 42,far;
+    far = (celsius * 9/5) + 32;
+    printf("the value of this celsius temperature in fahrenheit is %f",far);
+
+
+    return 0;
+}
