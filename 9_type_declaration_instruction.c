0bd526c92c0e69cb64887262ab13f42a3990d95f
Chetan Bhagat <cbhagat191@gmail.com>
2024-07-26 22:30:13 +0530

    Add files via upload

---
+++ 9_type_declaration_instruction.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 4,b,c; // type declaration instruction
+     c = b = a;
+    printf("the value of a is %d\n",a);
+    printf("the value of b is %d\n",b);
+    printf("the value of c is %d\n",c);
+    
+    // float a = 1.1 ;
+    // float b = a+8.9;   //=========>this will run
+    // printf("the value of b is %f",b);
+     
+    // float b = a+8.9;
+    // float a = 1.1;      //========>Error as we are trying to use a before defining it
+    // printf("the value of b is %f\n"b);
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
