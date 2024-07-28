34dbd56db3f7bc52001254d62986135d309c577c
Chetan Bhagat <cbhagat191@gmail.com>
2024-07-28 22:45:19 +0530

    Add files via upload

---
+++ I1_operator_precedence.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int x = 5;
+    int y = 6;
+    
+    printf("the value of 3*x - 8*y is %d\n", 3*x - 8*y);
+    printf("the value of 3*x / 8*y is %d\n", 3*x / 8*y);
+
+    return 0;
+    
+    
+    /*control instruction  
+    determines the flow of control in a program four types of control instruction in c are :-
+    1) sequence control instruction
+    2) decision control instruction
+    3) loop control instruction
+    4) case control instruction*/
+}
+
