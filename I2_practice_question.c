fad450f9168f8f27ab99076f5713feac53da3581
Chetan Bhagat <cbhagat191@gmail.com>
2024-07-30 22:39:24 +0530

    Add files via upload

---
+++ I2_practice_question.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+
+int main()
+{
+//   Q.3 write a program to determine whether a number is divisible by 97 or not
+    int num; 
+    printf("enter the number \n");
+    scanf("%d",&num);
+    printf("divisiblity test returns: %d\n",num%97);
+    
+    return 0;
+}
+
+ 
