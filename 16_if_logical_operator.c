fc3f348820dec9e329a728722becd5f5e6cbbc8a
Chetan Bhagat <cbhagat191@gmail.com>
2024-08-02 22:17:01 +0530

    Add files via upload

---
+++ 16_if_logical_operator.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    //int vippass = 1;
+    
+    printf("enter the age\n");
+    scanf("%d",&age);
+    
+    if((age <=70 && age>=18)) // ((age <=70 && age>=18) || (vippass=1))
+    {
+        printf("you are above 18 and below 70,you can drive\n");
+    }
+    
+    else
+    {
+        printf("you cannot drive");
+    }
+
+    return 0;
+}
+
