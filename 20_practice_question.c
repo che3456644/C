2c8e9c76e2e0c8d5fe9ebe072c22f202c94d8244
Chetan Bhagat <cbhagat191@gmail.com>
2024-08-26 22:50:04 +0530

    Add files via upload

---
+++ 20_practice_question.c
@@ -0,0 +1,20 @@
+// what will be the output of this program
+//int a=10;
+//if(a=11)
+//printf("i am 11");
+//else
+//printf("i am not 11");
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    if (a=11){
+        printf("i am 11");
+    }
+    else{
+        printf("i am not 11");
+    }
+
+    return 0;
+}
