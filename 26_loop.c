ddc417955fb78a91c12b34b500725de62b1c6b1d
Chetan Bhagat <cbhagat191@gmail.com>
2024-08-30 23:41:13 +0530

    Add files via upload

---
+++ 26_loop.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello\n");
+    printf("world\n");
+    //loops are used to repeat similar part of a code snippet efficiently
+    int a=1;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+
+    return 0;
+}
