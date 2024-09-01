aa980d19dbf48b523ad3bc69976348344595a280
Chetan Bhagat <cbhagat191@gmail.com>
2024-09-01 23:02:14 +0530

    Add files via upload

---
+++ 28_increment_operator.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i=5;
+    printf("the value after i++ is %d\n",++i);
+    /*
+    i++; -----> pehle print fir increment
+    ++i; -----> pehle increment fir print
+    */
+    printf("the value of i is %d\n",i);
+
+    return 0;
+}
